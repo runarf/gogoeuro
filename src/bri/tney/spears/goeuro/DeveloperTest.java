3a084262ce92294c43836216b384ecd64367a909
Runar Flåten <runar@tuta.io>
2015-10-26 16:55:26 +0100

    Cleaned up the class.

--- src/bri/tney/spears/goeuro/DeveloperTest.java
+++ src/bri/tney/spears/goeuro/DeveloperTest.java
@@ -1,11 +1,8 @@
 package bri.tney.spears.goeuro;
 
-import java.io.BufferedReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
@@ -17,58 +14,9 @@ import org.json.JSONTokener;
 import com.opencsv.CSVWriter;
 
 public class DeveloperTest {
-// randomm dd
-	public static void stringForm(URL url) {
-		HttpURLConnection conn = null;
-		BufferedReader rd = null;
-		StringBuilder sb = new StringBuilder();
-		String line;
-		JSONObject place = null;
-		CSVWriter writer = null;
-		JSONArray toJson = null;
-		String csv = "data.csv";
-		String record;
-		String[] csvRecord;
-
-		try {
-			conn = (HttpURLConnection) url.openConnection();
-			rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-			try {
-				while ((line = rd.readLine()) != null) {
-					sb.append(line);
-				}
-			} finally {
-				rd.close();
-			}
-		} catch (IOException e) {
-			System.out.println("Caught IOexception: " + e.getMessage());
-		} finally {
-			conn.disconnect();
-		}
-
-		// System.out.println(sb.toString());
-		// System.out.println(toJson.toString());
-
-		toJson = new JSONArray(sb.toString());
-		try {
-			writer = new CSVWriter(new FileWriter(csv));
-			try {
-				for (int i = 0; i < toJson.length(); i++) {
-
-					place = toJson.getJSONObject(i);
-					record = Integer.toString(place.getInt("_id")) + "#" + place.get("name") + "#" + place.get("type")
-							+ "#" + Double.toString(place.getJSONObject("geo_position").getDouble("latitude")) + "#"
-							+ Double.toString(place.getJSONObject("geo_position").getDouble("latitude"));
-					System.out.println(record);
-					csvRecord = record.split("#");
-					writer.writeNext(csvRecord);
-				}
-			} finally {
-				writer.close();
-			}
-		} catch (IOException e) {
-			System.out.println("IO exception");
-		}
+	
+	public static void extractDataFromJSON(String[] args) {
+		
 	}
 
 	public static void csvForm(URL url) {
@@ -117,7 +65,6 @@ public class DeveloperTest {
 			System.out.println("The URL " + url + " is malformed");
 		}
 		System.out.println(url);
-		// stringForm(url);
 		csvForm(url);
 
 	}
